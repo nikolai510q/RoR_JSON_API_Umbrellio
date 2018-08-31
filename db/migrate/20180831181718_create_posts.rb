4dda293023f949b1f82c04467f12427de8d3adc7
nikolai510 <meisterofcode@gmail.com>
2018-08-31 22:29:45 +0300

    add models and migrations(create users/posts/ratings)

---
+++ db/migrate/20180831181718_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :posts do |t|
+      t.integer :user_id, null: false, index: true
+      t.string :header, null: false
+      t.text :body, null: false
+      t.inet :author_ip, null: false
+      t.timestamps
+    end
+  end
+end

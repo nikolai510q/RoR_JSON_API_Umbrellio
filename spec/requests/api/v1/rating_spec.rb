bf2128c3697b3c5f68ee43372ac844fb9e1e5550
nikolai510 <meisterofcode@gmail.com>
2018-09-03 12:05:34 +0300

    merge branchs

--- spec/requests/api/v1/rating_spec.rb
+++ spec/requests/api/v1/rating_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 describe "Ratings API" do
   describe "POST #create" do
-    let(:post_object) { create(:post) }
+    let(:post_object) { create(:rating) }
 
     it 'with valid params' do
      [:post_id, :value].each do |param|
       post v1_ratings_url, params: { post_id: post_object.id, value: 5 }
       expect(response.status).to eq(200)
-      expect(json.keys).to include('id', 'average_rating')
-      expect(json['id']).to eq(post_object.id)
+      expect(json.keys).to include('post_id', 'value')
+      expect(json['post_id']).to eq(post_object.id)
     end
   end
 
